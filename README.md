8c2bd952afb8f610eae437d26e388e14a9caf575
KOUBIAKDZ <52467498+KOUBIAKDZ@users.noreply.github.com>
2019-07-03 12:28:00 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# porte-unicorn-ouvrir-et-fermer-
+#porte unicorn (ouvrir et fermer porte )
+
+
+REALISE PAR KOUBIAK
+
+
+
+Mon discord :  KOUBIAK (DEV)#3046
+
+
+
+
+Si vous voulez d'autre porte  de n'importe ou dites le moi en MP
---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+Config.Locale = 'fr'
+
+Config.DoorList = {
+
+	 
+	--
+	-- UNICORN  
+	--
+
+	---------------------------------------------------------------------UNICORN (Porte principal)
+	[1] = {
+		objName = "prop_strip_door_01",
+		objCoords  = {x = 127.95523834228516, y = -1298.50341796875, z = 29.41962242126465},
+		textCoords = {x = 127.9552, y = -1298.50341, z = 29.419},
+		locked = true,
+		distance = 12,
+		size = 2
+	},
+
+} 
